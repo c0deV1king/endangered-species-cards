0c15a7a59fd39d6e49058c10b824cc8bdb7b8e21
Austin Higgins <austinjhiggins@pm.me>
2024-07-05 13:10:17 -0230

    Created a card component and properly set up the card with the array information

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import './styles.css';
+import Card from './Card';
 
 // create a card class
-class Card {
+class CardBuilder {
   constructor(image, name, habitat, diet, endangeredLevel, rank) {
     this.image = image;
     this.name = name;
@@ -14,17 +15,29 @@ class Card {
 }
 
 // create new card objects
-const cards = [
-  new Card('https://i.imgur.com/xKkFQmB.png', 'Alaskan Kitty Shark', 'Alaskan waters', 'Fish, seals, and the occasional surfer', 'Critically Endangered', 1),
+const initialCards = [
+  new CardBuilder('https://i.imgur.com/xKkFQmB.png', 'Alaskan Kitty Shark', 'Alaskan waters', 'Fish, seals, and the occasional surfer', 'Critically Endangered', 1)
 ]
 
 // build a bit... break down into components
 export default function App() {
+  const [cards, setCards] = useState(initialCards);
+
+
   return (
-  <div className='App-wrapper'>
-    <div className='content'>
-      <img className='card-image' src='https://i.imgur.com/xKkFQmB.png' alt='kitty shark' />
-      <h1 className='animal-name'> Alaskan Kitty Shark </h1>
+    <div className='App-wrapper'>
+      <div className='card-list'>
+        {cards.map((card, index) => (
+          <Card
+            key={index}
+            image={card.image}
+            name={card.name}
+            habitat={card.habitat}
+            diet={card.diet}
+            endangeredLevel={card.endangeredLevel}
+            rank={card.rank}
+          />
+        ))}
+      </div>
     </div>
-  </div>
 )}
---
+++ src/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.css";
+
+// creating a function to layout a card
+function Card({ image, name, habitat, diet, endangeredLevel, rank }) {
+  return (
+    <div className="card">
+        <img className="card-image" src={image} alt={name} />
+        <h1 className="animal-name">{name}</h1>
+        <p className="animal-habitat">Habitat: {habitat}</p>
+        <p className="animal-diet">Diet: {diet}</p>
+        <p className="endangered-level">Endangered Level: {endangeredLevel}</p>
+        <p className="rank">Rank: {rank}</p>
+    </div>
+  );
+}
+
+export default Card;
